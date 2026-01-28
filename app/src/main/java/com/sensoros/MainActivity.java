424ed8bd3fbf384afcd6a1f486954e21d5717708
batmeezy918 <Batmeezy2024@gmail.com>
2026-01-27 22:06:35 -0600

    Add live battery telemetry

--- app/src/main/java/com/sensoros/MainActivity.java
+++ app/src/main/java/com/sensoros/MainActivity.java
@@ -1,11 +1,26 @@
 package com.sensoros;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.os.BatteryManager;
+import android.content.Context;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TextView tv = new TextView(this);
+        tv.setTextSize(20);
+
+        BatteryManager bm = (BatteryManager) getSystemService(Context.BATTERY_SERVICE);
+        int level = bm.getIntProperty(BatteryManager.BATTERY_PROPERTY_CAPACITY);
+
+        String msg = "SensorOS Live\n\nBattery Level: " + level + "%\nDevice Online";
+        tv.setText(msg);
+
+        setContentView(tv);
     }
 }
